70924e704814be0094fe3297b61c867abc9d4bb6
ortizfram <ortizfranco48@gmail.com>
2024-03-01 12:39:48 -0300

    course api controller

--- api/controllers/course.controller.js
+++ api/controllers/course.controller.js
@@ -88,23 +88,269 @@ export const courseCreate = async (req, res, next) => {
 
 // Update
 export const courseUpdate = async (req, res, next) => {
-  res.send("course Update");
+  console.log(req.body);
+  console.log(req.files);
+
+  try {
+    const courseId = req.params.id;
+
+    // Fetch existing course from the database
+    const course = await Course.findById(courseId);
+
+    if (!course) {
+      return next(errorHandler(400, `Course not found.`));
+    }
+
+    // Extract necessary data from request body
+    const {
+      title,
+      description,
+      text_content,
+      ars_price,
+      usd_price,
+      discount_ars,
+      discount_usd,
+      author: authorId,
+    } = req.body;
+
+    // Validate required fields
+    const requiredFields = [
+      "title",
+      "description",
+      "text_content",
+      "ars_price",
+      "usd_price",
+    ];
+    for (const field of requiredFields) {
+      if (!req.body[field]) {
+        return next(errorHandler(400, `The field '${field}' is required.`));
+      }
+    }
+
+    // Manage discount value
+    const discountArs = discount_ars || null;
+    const discountUsd = discount_usd || null;
+
+    // Update course details
+    const updatedCourse = await Course.findByIdAndUpdate(courseId, {
+      title,
+      description,
+      text_content,
+      ars_price,
+      usd_price,
+      discount_ars: discountArs,
+      discount_usd: discountUsd,
+    });
+
+    console.log("\nUpdating course...");
+    console.log("\nCourse:", updatedCourse);
+
+    // Redirect after updating the course
+    return res.status(200).json({
+      message: "Course updated successfully",
+    });
+  } catch (error) {
+    return next(error);
+  }
 };
 
 // courselist
 export const courselist = async (req, res, next) => {
-  res.send("course list");
+  try {
+    const message = req.query.message;
+    let user = req.session.user;
+    const isAdmin = user && user.role === "admin";
+
+    // Fetch all courses ordered by updated_at descending
+    const courses = await Course.find({})
+      .populate("author", "name username avatar") // Populate author details
+      .sort({ updated_at: -1 })
+      .lean(); // Convert Mongoose documents to plain JavaScript objects
+
+    console.log("courses: ", courses);
+
+    // Map courses to desired response format
+    let formattedCourses = courses.map((course) => {
+      return {
+        title: course.title,
+        slug: course.slug,
+        description: course.description,
+        ars_price: course.ars_price,
+        usd_price: course.usd_price,
+        discount_ars: course.discount_ars,
+        discount_usd: course.discount_usd,
+        thumbnail: course.thumbnail,
+        id: course._id.toString(),
+        thumbnailPath: course.thumbnail,
+        created_at: new Date(course.created_at).toLocaleString(),
+        updated_at: new Date(course.updated_at).toLocaleString(),
+        author: {
+          name: course.author.name,
+          username: course.author.username,
+          avatar: course.author.avatar,
+        },
+        next: `/api/course/${course._id}`, // Dynamic course link
+      };
+    });
+
+    // Filter out enrolled courses
+    if (user) {
+      const enrolledCourseIds = user.enrolledCourses.map((course) =>
+        course.toString()
+      );
+      formattedCourses = formattedCourses.filter(
+        (course) => !enrolledCourseIds.includes(course.id)
+      );
+    }
+
+    // Send courses response
+    res.status(200).json({
+      route: "courses",
+      title: "Cursos",
+      courses: formattedCourses,
+      totalItems: formattedCourses.length,
+      user,
+      message,
+      isAdmin,
+    });
+  } catch (error) {
+    console.log("error fetching courses");
+    next(error);
+  }
 };
 
 // courseOwned
 export const courseOwned = async (req, res, next) => {
-  res.send("course Owned");
+  try {
+    const message = req.query.message;
+    let user = req.session.user;
+    const isAdmin = user && user.role === "admin";
+
+    // Fetch courses owned by the user
+    let courses = [];
+    if (user) {
+      const enrolledCourseIds = await UserCourse.find({
+        user_id: user.id,
+      }).distinct("course_id");
+      courses = await Course.find({ _id: { $in: enrolledCourseIds } })
+        .populate("author", "name username avatar")
+        .sort({ updated_at: -1 })
+        .lean();
+    }
+
+    // Map courses to the desired response format
+    let formattedCourses = courses.map((course) => {
+      return {
+        id: course._id.toString(),
+        title: course.title,
+        slug: course.slug,
+        description: course.description,
+        ars_price: course.ars_price,
+        usd_price: course.usd_price,
+        discount_ars: course.discount_ars,
+        discount_usd: course.discount_usd,
+        thumbnail: course.thumbnail,
+        thumbnailPath: course.thumbnail,
+        created_at: new Date(course.created_at).toLocaleString(),
+        updated_at: new Date(course.updated_at).toLocaleString(),
+        author: {
+          name: course.author.name,
+          username: course.author.username,
+          avatar: course.author.avatar,
+        },
+        next: `/api/course/${course._id}`, // Dynamic course link
+      };
+    });
+
+    // Send courses response
+    res.status(200).json({
+      route: "courses",
+      title: "Cursos",
+      courses: formattedCourses,
+      totalItems: formattedCourses.length,
+      user,
+      message,
+      isAdmin,
+    });
+  } catch (error) {
+    console.log("Error fetching owned courses:");
+    next(error);
+  }
 };
 // courseDetail
 export const courseDetail = async (req, res, next) => {
-  res.send("course Detail");
+  const courseId = req.params.id;
+  const user = req.session.user || null;
+  const message = req.query.message;
+
+  try {
+    // Fetch course details including author information
+    const course = await Course.findById(courseId)
+      .populate("author", "name username avatar")
+      .lean();
+
+    if (!course) {
+      return next(errorHandler(400, `Course not found`));
+    }
+
+    // Format course timestamps
+    course.created_at = new Date(course.created_at).toLocaleString();
+    course.updated_at = new Date(course.updated_at).toLocaleString();
+
+    // Fetching author details
+    const author = await User.findById(course.author_id).lean();
+
+    if (!author) {
+      return next(errorHandler(404, `Author details not found`));
+    }
+
+    // Extend course with author details
+    course.author = {
+      name: author.name,
+      username: author.username,
+      avatar: author.avatar,
+    };
+
+    // Fetch enrolled courses for the user
+    let enrolledCourses = [];
+    if (user) {
+      const enrolledCoursesIds = await UserCourse.find({
+        user_id: user._id,
+      }).distinct("course_id");
+      enrolledCourses = await Course.find({
+        _id: { $in: enrolledCoursesIds },
+      }).lean();
+    }
+
+    // Send JSON response with the fetched data
+    res.json({
+      course,
+      message,
+      user,
+      enrolledCourses,
+    });
+  } catch (error) {
+    console.log("Error fetching the course");
+    next(error);
+  }
 };
 // courseDelete
 export const courseDelete = async (req, res, next) => {
-  res.send("course Delete");
+  try {
+    const courseId = req.params.id;
+
+    // Find the course by its ID and delete it
+    const deletedCourse = await Course.findByIdAndDelete(courseId);
+
+    // Check if the course was found and deleted
+    if (!deletedCourse) {
+      return next(errorHandler(404, "Course not found"));
+    }
+
+    // Respond with a success message
+    return res.status(200).json({ message: "Course deleted successfully" });
+  } catch (error) {
+    console.error("Error deleting course:");
+    return next(error)
+  }
 };
