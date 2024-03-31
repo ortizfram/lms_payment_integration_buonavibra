38288fa6f02f0fb2360bb77bd057c9366bc76c14
Franco Ortiz <ortizfranco48@yahoo.com>
2024-03-31 19:45:07 -0300

    loadMore courses

--- client/src/components/course/courseList.jsx
+++ client/src/components/course/courseList.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
 import AuthContext from "../../context/AuthContext";
 import "../../public/css/course/courses.css";
 import BtnAdminPreview from "./BtnAdminPreview";
@@ -7,100 +7,125 @@ function CourseList({ courses }) {
   const { currentUser } = useContext(AuthContext);
   const isAdmin = currentUser?.["isAdmin"];
 
+  // State for pagination
+  const [visibleCourses, setVisibleCourses] = useState(5); // Number of courses to initially display
+  const [loadMoreVisible, setLoadMoreVisible] = useState(true); // Control visibility of Load More button
+
+  const loadMore = () => {
+    // Increase the number of visible courses by 5
+    setVisibleCourses(prev => prev + 5);
+  };
+
   return (
-    <div>
-      <ul>
-        {courses.map((course, index) => (
-          <li key={index}>
-            <BtnAdminPreview courseId={course._id}/>
-            <div className="course-item position-relative backdrop-filter shadow-lg">
-              {/* DISCOUNT for ARS only */}
-              {course.discount_ars >= 1 && course.discount_usd < 1 && (
-                <p className="position-absolute start-40 top-10 translate-middle-x  translate-middle-y  text-center fw-lighter text-xs bg-success p-[0.1] rounded">
-                  ARS {course.discount_ars}%OFF
-                </p>
-              )}
-              {/* DISCOUNT for USD only */}
-              {course.discount_usd >= 1 && course.discount_ars < 1 && (
-                <p className="position-absolute start-40 top-10 translate-middle-x translate-middle-y  text-center fw-lighter text-xs bg-success p-[0.1] rounded">
-                  USD ${course.discount_usd}%OFF
-                </p>
-              )}
-              {/* DISCOUNT for both ARS and USD */}
-              {course.discount_ars >= 1 && course.discount_usd >= 1 && (
-                <div>
-                  <p className="position-absolute translate-middle-x translate-middle-y  text-center fw-lighter text-xs bg-success p-[0.1] rounded">
-                    ARS {course.discount_ars}% USD ${course.discount_usd}%
+    <div className="courses-page-container">
+      <div className="courses-container">
+        <ul className="courses-grid">
+          {courses.slice(0, visibleCourses).map((course, index) => (
+            <li key={index}>
+              <BtnAdminPreview courseId={course._id} />
+              <div className="course-item position-relative backdrop-filter shadow-lg">
+                {/* DISCOUNT for ARS only */}
+                {course.discount_ars >= 1 && course.discount_usd < 1 && (
+                  <p className="position-absolute start-40 top-10 translate-middle-x  translate-middle-y  text-center fw-lighter text-xs bg-success p-[0.1] rounded">
+                    ARS {course.discount_ars}%OFF
                   </p>
-                </div>
-              )}
-
-              {/* NExt Link */}
-              <a href={`/course/enroll/${course._id}`}>
-                {/* COURSE DATA */}
-                <img src={course.thumbnail} alt={`thumbnail-${course.slug}`} />
-                <p className="timestamp ">{course.updated_at}</p>
-
-                {/* AUTHOR */}
-                <div className="author">
-                  {course.author && course.author.avatar && (
-                    <img
-                      src={course.author.avatar}
-                      alt="User Avatar"
-                      className="avatar"
-                    />
-                  )}
-                  {course.author && (
-                    <p className="aut">
-                      <strong>{course.author.username}</strong> •{" "}
-                      {course.author.name}
-                    </p>
-                  )}
-                </div>
-                <h2 className="">{course.title}</h2>
-
-                {/* PRICE */}
-                {course.usd_price || course.ars_price ? (
-                  <p className="">
-                    USD {course.usd_price} | ARS {course.ars_price}
-                  </p>
-                ) : null}
-
-                {/* DESCRIPTION */}
-                {course.description && (
-                  <p className="">{course.description} </p>
                 )}
-              </a>
+                {/* DISCOUNT for USD only */}
+                {course.discount_usd >= 1 && course.discount_ars < 1 && (
+                  <p className="position-absolute start-40 top-10 translate-middle-x translate-middle-y  text-center fw-lighter text-xs bg-success p-[0.1] rounded">
+                    USD ${course.discount_usd}%OFF
+                  </p>
+                )}
+                {/* DISCOUNT for both ARS and USD */}
+                {course.discount_ars >= 1 && course.discount_usd >= 1 && (
+                  <div>
+                    <p className="position-absolute translate-middle-x translate-middle-y  text-center fw-lighter text-xs bg-success p-[0.1] rounded">
+                      ARS {course.discount_ars}% USD ${course.discount_usd}%
+                    </p>
+                  </div>
+                )}
 
-              {/* ADMIN OPTIONS*/}
-              {/* Assuming currentUser is passed as a prop */}
-              {isAdmin === true && (
-                <div className="course-actions">
-                  {/* UPDATE */}
-                  <p className="">
-                    <a
-                      className="text-muted"
-                      href={`/api/course/update/${course._id}`}
-                    >
-                      <i className="fas fa-edit me-2"></i>
-                    </a>
-                    <input type="hidden" name="author" value={course.author} />
-                  </p>
-                  {/* DEL */}
-                  <p>
-                    <a
-                      className="text-muted"
-                      href={`/api/course/delete/${course._id}`}
-                    >
-                      <i className="fas fa-trash-alt me-2"></i>
-                    </a>
-                  </p>
-                </div>
-              )}
-            </div>
-          </li>
-        ))}
-      </ul>
+                {/* NExt Link */}
+                <a href={`/course/enroll/${course._id}`}>
+                  {/* COURSE DATA */}
+                  <img
+                    src={course.thumbnail}
+                    alt={`thumbnail-${course.slug}`}
+                  />
+                  <p className="timestamp ">{course.updated_at}</p>
+
+                  {/* AUTHOR */}
+                  <div className="author">
+                    {course.author && course.author.avatar && (
+                      <img
+                        src={course.author.avatar}
+                        alt="User Avatar"
+                        className="avatar"
+                      />
+                    )}
+                    {course.author && (
+                      <p className="aut">
+                        <strong>{course.author.username}</strong> •{" "}
+                        {course.author.email}
+                      </p>
+                    )}
+                  </div>
+                  <h2 className="course-title">{course.title}</h2>
+
+                  {/* PRICE */}
+                  {course.usd_price || course.ars_price ? (
+                    <p className="">
+                      USD {course.usd_price} | ARS {course.ars_price}
+                    </p>
+                  ) : null}
+
+                  {/* DESCRIPTION */}
+                  {course.description && (
+                    <p className="">{course.description} </p>
+                  )}
+                </a>
+
+                {/* ADMIN OPTIONS*/}
+                {/* Assuming currentUser is passed as a prop */}
+                {isAdmin === true && (
+                  <div className="course-actions">
+                    {/* UPDATE */}
+                    <p className="">
+                      <a
+                        className="text-muted"
+                        href={`/api/course/update/${course._id}`}
+                      >
+                        <i className="fas fa-edit me-2"></i>
+                      </a>
+                      <input
+                        type="hidden"
+                        name="author"
+                        value={course.author}
+                      />
+                    </p>
+                    {/* DEL */}
+                    <p>
+                      <a
+                        className="text-muted"
+                        href={`/api/course/delete/${course._id}`}
+                      >
+                        <i className="fas fa-trash-alt me-2"></i>
+                      </a>
+                    </p>
+                  </div>
+                )}
+              </div>
+            </li>
+          ))}
+        </ul>
+        {loadMoreVisible && visibleCourses < courses.length && (
+          <div className="text-center mt-4">
+            <button className="btn btn-primary" onClick={loadMore}>
+              Load More
+            </button>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
--- client/src/components/course/courseOwnedList.jsx
+++ client/src/components/course/courseOwnedList.jsx
@@ -1,82 +1,103 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
 import AuthContext from "../../context/AuthContext";
 import "../../public/css/course/courses.css";
 
-function courseOwnedList({ courses }) {
+function CourseOwnedList({ courses }) {
   const { currentUser } = useContext(AuthContext);
   const isAdmin = currentUser?.["isAdmin"];
 
+  // State for pagination
+  const [visibleCourses, setVisibleCourses] = useState(5); // Number of courses to initially display
+  const [loadMoreVisible, setLoadMoreVisible] = useState(true); // Control visibility of Load More button
+
+  const loadMore = () => {
+    // Increase the number of visible courses by 5
+    setVisibleCourses(prev => prev + 5);
+  };
+
   return (
-    <div>
-      <ul>
-        {courses.map((course, index) => (
-          <li key={index}>
-            <div className="course-item position-relative backdrop-filter shadow-lg">
-             
+    <div className="library-page-container">
+      <div className="courses-container">
+        <ul className="courses-grid">
+          {courses.slice(0, visibleCourses).map((course, index) => (
+            <li key={index}>
+              <div className="course-item position-relative backdrop-filter shadow-lg">
+                {/* NExt Link */}
+                <a href={`/course/${course._id}`}>
+                  {/* COURSE DATA */}
+                  <img
+                    src={course.thumbnail}
+                    alt={`thumbnail-${course.slug}`}
+                  />
+                  <p className="timestamp">{course.updated_at}</p>
 
-              {/* NExt Link */}
-              <a href={`/course/${course._id}`}>
-                {/* COURSE DATA */}
-                <img src={course.thumbnail} alt={`thumbnail-${course.slug}`} />
-                <p className="timestamp">{course.updated_at}</p>
+                  {/* AUTHOR */}
+                  <div className="author">
+                    {course.author && course.author.avatar && (
+                      <img
+                        src={course.author.avatar}
+                        alt="User Avatar"
+                        className="avatar"
+                      />
+                    )}
+                    {course.author && (
+                      <p className="author-info ">
+                        <strong>{course.author.username}</strong> •{" "}
+                        {course.author.name}
+                      </p>
+                    )}
+                  </div>
+                  <h2 className="">{course.title}</h2>
 
-                {/* AUTHOR */}
-                <div className="author">
-                  {course.author && course.author.avatar && (
-                    <img
-                      src={course.author.avatar}
-                      alt="User Avatar"
-                      className="avatar"
-                    />
+                  {/* DESCRIPTION */}
+                  {course.description && (
+                    <p className="">{course.description} </p>
                   )}
-                  {course.author && (
-                    <p className="author-info ">
-                      <strong>{course.author.username}</strong> •{" "}
-                      {course.author.name}
+                </a>
+
+                {/* ADMIN OPTIONS*/}
+                {/* Assuming currentUser is passed as a prop */}
+                {isAdmin === true && (
+                  <div className="course-actions">
+                    {/* UPDATE */}
+                    <p className="">
+                      <a
+                        className="text-muted"
+                        href={`/api/course/update/${course._id}`}
+                      >
+                        <i className="fas fa-edit me-2"></i>
+                      </a>
+                      <input
+                        type="hidden"
+                        name="author"
+                        value={course.author}
+                      />
                     </p>
-                  )}
-                </div>
-                <h2 className="">{course.title}</h2>
-
-              
-
-                {/* DESCRIPTION */}
-                {course.description && (
-                  <p className="">{course.description} </p>
+                    {/* DEL */}
+                    <p>
+                      <a
+                        className="text-muted"
+                        href={`/api/course/delete/${course._id}`}
+                      >
+                        <i className="fas fa-trash-alt me-2"></i>
+                      </a>
+                    </p>
+                  </div>
                 )}
-              </a>
-
-              {/* ADMIN OPTIONS*/}
-              {/* Assuming currentUser is passed as a prop */}
-              {isAdmin === true && (
-                <div className="course-actions">
-                  {/* UPDATE */}
-                  <p className="">
-                    <a
-                      className="text-muted"
-                      href={`/api/course/update/${course._id}`}
-                    >
-                      <i className="fas fa-edit me-2"></i>
-                    </a>
-                    <input type="hidden" name="author" value={course.author} />
-                  </p>
-                  {/* DEL */}
-                  <p>
-                    <a
-                      className="text-muted"
-                      href={`/api/course/delete/${course._id}`}
-                    >
-                      <i className="fas fa-trash-alt me-2"></i>
-                    </a>
-                  </p>
-                </div>
-              )}
-            </div>
-          </li>
-        ))}
-      </ul>
+              </div>
+            </li>
+          ))}
+        </ul>
+        {loadMoreVisible && visibleCourses < courses.length && (
+          <div className="text-center mt-4">
+            <button className="btn btn-primary" onClick={loadMore}>
+              Load More
+            </button>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
 
-export default courseOwnedList ;
+export default CourseOwnedList;
