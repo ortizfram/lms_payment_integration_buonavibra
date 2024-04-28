afa2b9edf5f275133da577c7c78484f75e1cbc94
Franco Ortiz <ortizfranco48@yahoo.com>
2024-04-28 13:15:06 -0300

    course update

--- client/src/pages/CourseUpdate.jsx
+++ client/src/pages/CourseUpdate.jsx
@@ -6,6 +6,7 @@ import { BACKEND_URL } from "../config.js";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+
 const CourseUpdate = () => {
   const { currentUser } = useContext(AuthContext);
   const { id } = useParams();
@@ -20,10 +21,7 @@ const CourseUpdate = () => {
     discount_ars: 0,
     discount_usd: 0,
   });
-  
   const $image = useRef(null);
-  let $file = useRef(null);
-  let $videoFile = useRef(null);
 
   useEffect(() => {
     const fetchCourseData = async () => {
@@ -33,6 +31,7 @@ const CourseUpdate = () => {
           const data = await response.json();
           const courseData = data.course;
           setFormData(courseData);
+          
         } else {
           throw new Error("Failed to fetch course data");
         }
@@ -73,15 +72,15 @@ const CourseUpdate = () => {
       body: formData,
     });
 
-    if (response.ok) {
+    if (response.status ===200) {
       const data = await response.json();
       setSuccessMessage(data.message);
-      console.log("course updated successfully");
-      toast.success("Actualizaste un curso");
+      // Redirect to the updated course page
+      toast.success("Curso ha sido actualizado");
       setTimeout(() => {
-        // Redirect to the updated course page
+        // navigate(`/`);
         window.location.href = data.redirectUrl;
-      }, 200);
+      }, 2000);
     } else {
       const errorData = await response.json();
       setErrorMessage(errorData.message);
@@ -92,162 +91,204 @@ const CourseUpdate = () => {
     formData && (
       <div className="courseCreate-page-cont min-w-[100vw]">
         <div className="max-w-[80vw] px-3 mt-[5vh] mx-auto">
-          <h1 className="text-3xl font-semibold text-gray-800 mb-4">
-            Actualizando Curso
-          </h1>
-          <form onSubmit={handleSubmit} className="space-y-4">
-            {errorMessage && (
-              <p className="text-red-500 text-center text-2xl font-semibold">
-                {errorMessage}
-              </p>
-            )}
-            {/* CONTENT */}
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          {errorMessage && (
+            <p className="text-red-500 text-center text-2xl font-semibold">
+              {errorMessage}
+            </p>
+          )}
+          {successMessage && (
+            <p className="text-green-500 text-center text-2xl font-semibold">
+              {successMessage}
+            </p>
+          )}
+          <div>
+            
+            <form onSubmit={handleSubmit} className="space-y-4">
+              {/* CONTENT */}
+              <h3>Titulo & contenido:</h3>
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                <div>
+                  <label
+                    htmlFor="title"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    titulo:
+                  </label>
+                  <input
+                    name="title"
+                    value={formData.title}
+                    onChange={handleChange}
+                    type="text"
+                    className="text-black mt-1 p-2 w-full border border-gray-300 rounded-md"
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="description"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Descripcion:
+                  </label>
+                  <input
+                    name="description"
+                    value={formData.description}
+                    onChange={handleChange}
+                    type="text"
+                    className="text-black mt-1 p-2 w-full border border-gray-300 rounded-md"
+                  />
+                </div>
+              </div>
               <div>
-                <input
-                  name="title"
-                  placeholder="Titulo"
-                  value={formData.title}
+                <label
+                  htmlFor="text_content"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  contenido texto:
+                </label>
+                <textarea
+                  name="text_content"
+                  value={formData.text_content}
                   onChange={handleChange}
                   type="text"
-                  className="text-black mt-1 p-2 w-full border border-black rounded-md"
-                />
+                  className="text-black mt-1 p-2 w-full border border-gray-300 rounded-md"
+                ></textarea>
               </div>
-              <div>
-                <input
-                  name="description"
-                  placeholder="Descripcion corta del curso"
-                  value={formData.description}
-                  onChange={handleChange}
-                  type="text"
-                  className="text-black mt-1 p-2 w-full border border-black rounded-md"
-                />
+              <hr />
+
+              {/* UPLOAD */}
+              <h3>Subir Archivos:</h3>
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                <div>
+                  <label
+                    htmlFor="video"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    subir video :
+                  </label>
+                  <input
+                    type="file"
+                    name="video"
+                    accept="video/*"
+                    onChange={handleChange}
+                    className="text-black"
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="image"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    subir miniatura:
+                  </label>
+                  <input
+                    type="file"
+                    id="file"
+                    name="image"
+                    accept="image/*"
+                    onChange={handleChange}
+                    className="text-black"
+                  />
+                </div>
               </div>
-            </div>
+              <div className="col-span-2">
+                <div className="preview">
+                  <img
+                    id="img"
+                    ref={$image}
+                    className="mt-4"
+                    style={{ width: 300 }}
+                  />
+                </div>
+              </div>
+              <hr />
 
-            <div>
-              <textarea
-                name="text_content"
-                value={formData.text_content}
-                placeholder="Contenido de texto"
-                onChange={handleChange}
-                type="text"
-                className="text-black mt-1 p-2 w-full border border-black rounded-md"
-              ></textarea>
-            </div>
+              {/* PRICE */}
+              <h3>Configurar precio</h3>
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                <div>
+                  <label
+                    htmlFor="ars_price"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    ARS Price:
+                  </label>
+                  <input
+                    type="number"
+                    id="ars_price"
+                    name="ars_price"
+                    value={formData.ars_price}
+                    onChange={handleChange}
+                    className="text-black mt-1 p-2 w-full border border-gray-300 rounded-md"
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="usd_price"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    USD Price:
+                  </label>
+                  <input
+                    type="number"
+                    id="usd_price"
+                    name="usd_price"
+                    value={formData.usd_price}
+                    onChange={handleChange}
+                    className="text-black mt-1 p-2 w-full border border-gray-300 rounded-md"
+                  />
+                </div>
+              </div>
+              <hr />
 
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-              <div>
-                <input
-                  type="file"
-                  name="video"
-                  accept="video/mp4,video/x-m4v,video/*"
-                  onChange={handleChange}
-                  className="hidden"
-                  ref={$videoFile}
-                />
+              {/* DISCOUNT */}
+              <h3>Adicionales & descuentos</h3>
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                <div>
+                  <label
+                    htmlFor="discount_ars"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    descuento_ars opcional (numeros enteros):
+                  </label>
+                  <br />
+                  <strong>% ARS </strong>
+                  <input
+                    type="number"
+                    id="discount_ars"
+                    name="discount_ars"
+                    value={formData.discount_ars}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="discount_usd"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    descuento_usd opcional (numeros enteros):
+                  </label>
+                  <br />
+                  <strong>% USD </strong>
+                  <input
+                    type="number"
+                    id="discount_usd"
+                    name="discount_usd"
+                    value={formData.discount_usd}
+                    onChange={handleChange}
+                  />
+                </div>
+              </div>
+              <input type="hidden" name="author_id" value={currentUser._id} />
+              <div className="flex justify-center mt-6">
                 <button
-                  onClick={() => $videoFile.current.click()} // Trigger file input click
-                  className="text-black mt-1 p-2 w-full border border-black rounded-md bg-gray-300 hover:bg-gray-200"
+                  type="submit"
+                  className="px-4 py-2 mb-5 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
                 >
-                  Seleccione un video
+                  Update Course
                 </button>
               </div>
-              <div>
-                <input
-                  type="file"
-                  id="file"
-                  name="image"
-                  accept="image/*"
-                  onChange={handleChange}
-                  className="hidden"
-                  ref={$file}
-                />
-                <button
-                  onClick={() => $file.current.click()} // Trigger file input click
-                  className="text-black mt-1 p-2 w-full border border-black rounded-md bg-gray-300 hover:bg-gray-200"
-                >
-                  Seleccione una miniatura
-                </button>
-              </div>
-            </div>
-
-            <div className="col-span-2">
-              <div className="preview">
-                <img
-                  id="img"
-                  ref={$image}
-                  className="mt-4"
-                  style={{ width: 300 }}
-                />
-              </div>
-            </div>
-
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-              <div>
-                <input
-                  type="number"
-                  id="ars_price"
-                  name="ars_price"
-                  value={formData.ars_price}
-                  placeholder="ARS precio"
-                  onChange={handleChange}
-                  className="text-black mt-1 p-2 w-full border border-black rounded-md"
-                />
-              </div>
-              <div>
-                <input
-                  type="number"
-                  id="usd_price"
-                  name="usd_price"
-                  value={formData.usd_price}
-                  placeholder="USD precio"
-                  onChange={handleChange}
-                  className="text-black mt-1 p-2 w-full border border-black rounded-md"
-                />
-              </div>
-            </div>
-
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-              <div>
-                <input
-                  type="number"
-                  id="discount_ars"
-                  placeholder="Descuento ARS (solo en enteros)"
-                  name="discount_ars"
-                  value={formData.discount_ars}
-                  onChange={handleChange}
-                  className="text-black mt-1 p-2 w-full border border-black rounded-md"
-                />
-              </div>
-              <div>
-                <input
-                  type="number"
-                  id="discount_usd"
-                  placeholder="Descuento USD (solo en enteros)"
-                  name="discount_usd"
-                  value={formData.discount_usd}
-                  onChange={handleChange}
-                  className="text-black mt-1 p-2 w-full border border-black rounded-md"
-                />
-              </div>
-            </div>
-
-            <input type="hidden" name="author_id" value={currentUser._id} />
-            {errorMessage && (
-              <p className="text-red-500 text-center">{errorMessage}</p>
-            )}
-            <div className="flex justify-center mt-6">
-              <button
-                type="submit"
-                className="px-4 py-2 mb-5 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
-              >
-                Update Course
-              </button>
-            </div>
-          </form>
-
-          <div></div>
+            </form>
+          </div>
         </div>
       </div>
     )
