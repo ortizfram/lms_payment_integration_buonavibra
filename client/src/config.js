76779a9c45ac05494ca4d46750ba2d382b8875b0
Franco Ortiz <ortizfranco48@yahoo.com>
2024-04-30 11:30:44 -0300

    🚀env config

--- client/src/config.js
+++ client/src/config.js
@@ -2,4 +2,4 @@
 
 export const isDev = process.env.NODE_ENV === "development";
 export const BACKEND_URL = isDev ? "http://localhost:2020" : "https://api-buonavibra-prod.onrender.com"//import.meta.env.VITE_REACT_APP_BACKEND_URL;
-export const FRONTEND_URL = isDev ? "http://localhost:5173" : import.meta.env.VITE_REACT_APP_FRONTEND_URL;
+export const FRONTEND_URL = isDev ? "http://localhost:5173" : "https://buonavibra.com.ar"//import.meta.env.VITE_REACT_APP_FRONTEND_URL;
