89e73d0c22a9bc752db53a59f1717e075783913b
Franco Ortiz <ortizfranco48@yahoo.com>
2024-04-30 13:33:47 -0300

    a

--- client/src/pages/Thanks.jsx
+++ client/src/pages/Thanks.jsx
@@ -1,25 +1,12 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext } from "react";
 import { BACKEND_URL } from "../config";
 import axios from "axios";
 import AuthContext from "../context/AuthContext";
-import Loader from "../components/loader/Loader";
 
 function Thanks() {
   const { currentUser } = useContext(AuthContext);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    if (currentUser) {
-      setLoading(false);
-    }
-  }, [currentUser]);
 
   const asignMPandGoCourses = async () => {
-    if (!currentUser) {
-      // Handle case where currentUser is not available yet
-      return;
-    }
-
     try {
       const response = await axios.post(
         `${BACKEND_URL}/api/membership/success-mp`,
@@ -34,16 +21,16 @@ function Thanks() {
   };
 
   return (
-      <div className="text-center mt-[40vh]">
-        ❤️✨ Gracias por tu compra ✨❤️
-        <br />
-        <button
-          onClick={asignMPandGoCourses}
-          className="text-white bg-success text-center p-3 rounded-lg mt-3"
-        >
-          Ir a Cursos
-        </button>
-      </div>
+    <div className="text-center mt-[40vh]">
+      ❤️✨ Gracias por tu compra ✨❤️
+      <br />
+      <button
+        onClick={asignMPandGoCourses}
+        className="text-white bg-success text-center p-3 rounded-lg mt-3"
+      >
+        Ir a Cursos
+      </button>
+    </div>
   );
 }
 
