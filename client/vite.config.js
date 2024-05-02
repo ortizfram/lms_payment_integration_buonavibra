edf3c7174c8dcfd2757b84d7ea339d79450d306a
Franco Ortiz <ortizfranco48@yahoo.com>
2024-05-02 14:44:53 -0300

    🚀 vite target

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import {BACKEND_URL} from "./src/config.js"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:2020',
+        target:  BACKEND_URL,
+        // target:  `'http://localhost:2020'`,
         secure: false,
       },
     },
