20c44ab8640028417fd0ded32e84f3e02eb05625
Franco Ortiz <ortizfranco48@yahoo.com>
2024-06-13 16:02:53 -0300

    font alifira

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        alifira: ['Alifira', 'sans-serif'],
+        briemhand: ['Briem Hand', 'cursive'],
+      },
+    }
   },
   plugins: [],
 }
