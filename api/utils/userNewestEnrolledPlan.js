459a16555320c0ba24667c4a9d301a763e66bddc
Franco Ortiz <ortizfranco48@yahoo.com>
2024-06-13 13:37:17 -0300

    a

--- api/utils/userNewestEnrolledPlan.js
+++ api/utils/userNewestEnrolledPlan.js
@@ -2,16 +2,19 @@ import mongoose from "mongoose";
 import UserPlan from "../models/user_plan.model.js";
 
 export const getNewestEnrolledPlan = async (userId) => {
-    console.log(`getNewestEnrolledPlan(${userId})`)
   try {
     // Find the newest enrolled plan for the user
     let newestEnrolledPlan = await UserPlan.find({ user_id: new mongoose.Types.ObjectId(userId) })
       .sort({ createdAt: -1 })
       .populate("plan_id"); // Populate the plan details if needed
-      console.log("result ", newestEnrolledPlan)
 
-       // Extract the first plan_id if the array is not empty
-    const planId = newestEnrolledPlan.length > 0 ? newestEnrolledPlan[0].plan_id._id : [];
+    // Check if newestEnrolledPlan is null or empty
+    if (!newestEnrolledPlan || newestEnrolledPlan.length === 0) {
+      return null; // or handle appropriately based on your application logic
+    }
+
+    // Extract the first plan_id if the array is not empty
+    const planId = newestEnrolledPlan[0].plan_id._id;
 
     return planId;
   } catch (error) {
